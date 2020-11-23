25ac05ad786a9ec8044cc8967825087429d53f12
Matt <Geczy@users.noreply.github.com>
2020-11-23 18:01:44 -0500

    D D D D D DARK MODE??

--- tip_script.py
+++ tip_script.py
@@ -6,6 +6,13 @@ hide_streamlit_style = """
 #MainMenu {visibility: hidden;}
 footer {visibility: hidden;}
 </style>
+<script src="https://cdn.jsdelivr.net/npm/darkmode-js@1.5.7/lib/darkmode-js.min.js"></script>
+<script>
+  function addDarkmodeWidget() {
+    new Darkmode().showWidget();
+  }
+  window.addEventListener('load', addDarkmodeWidget);
+</script>
 
 """
 # st.markdown(hide_streamlit_style, unsafe_allow_html=True)
