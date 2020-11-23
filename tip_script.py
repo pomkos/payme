5022a9d59dde796e851fa4984afdc393f7414620
pomkos <albeithome@gmail.com>
2020-11-23 12:38:49 -0500

    stupid 0.85999999999999 bug

--- tip_script.py
+++ tip_script.py
@@ -71,6 +71,7 @@ def venmo_requester(my_dic, total, tax=0, tip=0, misc_fees=0):
         precheck_sum += sum(my_dic[key])
     
     precheck_sum = round(precheck_sum+tax+tip+misc_fees,2)
+    total = round(total,2)
     if total != precheck_sum:
         return st.write(f"You provided {total} as the total, but I calculated {precheck_sum}")
     else:
