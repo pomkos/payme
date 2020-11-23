111fd44e991da93944f31d9fe77b3f5397ede23b
pomkos <albeithome@gmail.com>
2020-11-23 16:58:41 +0000

    updated equation, no more rounding error. Hid dev mode

--- tip_script.py
+++ tip_script.py
@@ -8,7 +8,7 @@ footer {visibility: hidden;}
 </style>
 
 """
-# st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
 def db_save_table(dataframe, name, db = 'money_split.db', folder='sqlite:///C:\\Users\\albei\\OneDrive\\Desktop\\streamlit_test\\', if_exists='fail', index=False):
     '''
@@ -71,7 +71,7 @@ def venmo_requester(my_dic, total, tax=0, tip=0, misc_fees=0):
         precheck_sum += sum(my_dic[key])
     
     precheck_sum = round(precheck_sum+tax+tip+misc_fees,2)
-    if round(total,2) != precheck_sum:
+    if total != precheck_sum:
         return st.write(f"You provided {total} as the total, but I calculated {precheck_sum}")
     else:
         num_ppl = len(my_dic.keys())
