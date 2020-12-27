bcb7c41f9505352c0bbff0b5c989b62c5a292aa7
pomkos <albeithome@gmail.com>
2020-12-27 17:30:33 +0000

    catch db error, inform user db is not function rn

--- payme.py
+++ payme.py
@@ -62,21 +62,25 @@ def start(button=None):
         st.write("Not required, but very fun")
         col_save,col_show = st.beta_columns([0.33,1])
 
-    with col_show:
-        button_show = st.button(label='Preview the Database')
-    with col_save:
-        button_save = st.button(label='Submit to Database')
-    if button_save == True:
-        saveus = saveInfo(my_total, my_food, tip_perc, tax_perc, fee_part,showme='no')
-        saveus.save_table()
-        st.balloons()
+        with col_show:
+            button_show = st.button(label='Preview the Database')
+        with col_save:
+            button_save = st.button(label='Submit to Database')
 
-    if button_show == True:
-        showus = saveInfo()
-        dataframe = showus.read_table()
-        dataframe = dataframe.iloc[:,1:]
-        show_me = dataframe.tail()
-        st.table(show_me)
+        try:
+            if button_save == True:
+                saveus = saveInfo(my_total, my_food, tip_perc, tax_perc, fee_part,showme='no')
+                saveus.save_table()
+                st.balloons()
+
+            if button_show == True:
+                showus = saveInfo()
+                dataframe = showus.read_table()
+                dataframe = dataframe.iloc[:,1:]
+                show_me = dataframe.tail()
+                st.table(show_me)
+        except:
+            st.info("Database function down, come back later.")
         
     ###################
     # TESTING GROUNDS #
