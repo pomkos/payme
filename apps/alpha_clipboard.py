16f4f48d497771a4a5fc68d78997153263416e05
Csillag <csillagok@gmail.com>
2020-12-31 16:07:53 +0000

    copy button by Mom!

--- apps/alpha_clipboard.py
+++ apps/alpha_clipboard.py
@@ -1,6 +1,6 @@
 import streamlit as st
-# archive from trying to make one-click copy to clipboard a reality
 
+st.button("Click me")
 def set_params(my_dic, total, tax, tip, misc_fees, view, share=False):
     'Adds parameters to url'
     st.experimental_set_query_params(
@@ -59,38 +59,39 @@ def read_from_clipboard():
             df = pd.read_csv(StringIO(result.get("GET_TEXT")))
             st.table(df)
     
-def copy_to_clipboard(text):
+def copy_to_clipboard(text=''):
     import streamlit as st
     from bokeh.models.widgets import Button
     from bokeh.models import CustomJS
     from streamlit_bokeh_events import streamlit_bokeh_events
     from io import StringIO
     import pandas as pd
-    
+    import js2py
+    import streamlit.components.v1 as components
+           
     copy_button = Button(label="Copy All of Me")
     
-    html_code = f"""<!-- The text field -->
-    <input type="text" value="{text.replace(" ","%20")}" id="myInput">
-
-    <!-- The button used to copy the text -->
-    <button onclick="myFunction()">Copy text</button>"""
-    
-    js_code = """function myFunction() {
-    /* Get the text field */
+    html_code =components.html(
+        """
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <div><input type="text" value=" " id="myInput">
+    <button onclick="myFunction()">Copy text</button></div>
+    <SCRIPT LANGUAGE="JavaScript">
+    function myFunction()
+    {
     var copyText = document.getElementById("myInput");
-
-    /* Select the text field */
     copyText.select();
-    copyText.setSelectionRange(0, 99999); /* For mobile devices */
-
-    /* Copy the text inside the text field */
+    copyText.setSelectionRange(0, 99999); 
     document.execCommand("copy");
-
-    /* Alert the copied text */
     alert("Copied the text: " + copyText.value);
-    }"""
+    }
+    </SCRIPT>
+                         """)
+                               
     st.write(html_code, unsafe_allow_html=True )
-    copy_button.js_on_event("button_click", CustomJS(code=js_code))
+    copy_button.js_on_event("button_click")
+    
     
 def tg_formatter(link_output):
     '''
@@ -122,5 +123,7 @@ def tg_formatter(link_output):
    
     ### Copy to clipboard, for easy pasting to tg ###
     
-    # tg_open = f'tg://msg?text={message}'.replace(" ","%20")
-    # st.write(f'<a href="{tg_open}" target="_blank" rel="noopener noreferrer">Click me for TG</a>',unsafe_allow_html=True)
+    tg_open = f'tg://msg?text={message}'.replace(" ","%20")
+    st.write(f'<a href="{tg_open}" target="_blank" rel="noopener noreferrer">Click me for TG</a>',unsafe_allow_html=True)
+    
+copy_to_clipboard()
