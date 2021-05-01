21115c210f40de28580454276286c0acd2061670
pomkos <albeithome@gmail.com>
2021-05-01 20:54:47 +0000

    add meals one at a time

--- apps/food_select.py
+++ apps/food_select.py
@@ -13,29 +13,31 @@ cnx = engine.connect()
 
 with st.beta_expander("How to"):
     st.write("""
-    * Choose name
-    * Choose all meals you had
-    * Enter how many of each meal you had separated by commas.
-        Ex: `1,2,0.5`
-    * Use fractions to indicate shared orders. 0.33 if split by three, 0.5 if split by two, etc.
-    * Click `Calculate`. If everything looks good click `Confirm and Submit`
+    1. Choose name
+    1. Choose a food item
+    1. Enter how many of that meal you had.
+    1. Use fractions to indicate shared orders. 0.33 if split by three, 0.5 if split by two, etc.
+    1. Click `Calculate`. If everything looks good click `Confirm and Submit`
     """)
 
 # dictionary where keys are the food, values are a list of [price, num_ordered]
 food_dict = {
-    "infused shot (13)": [6, 13],
-    "lavender fusion (2)": [9, 2],
-    "moskow mule (1)": [9, 1],
-    "eggplant appetizer (2)": [8, 2],
-    "vareniki + potato and mushrooms (2)": [17, 2],
-    "fresh red salmon caviar karat (1)": [35, 1],
-    "blini with stuffing (salmon) (1)": [12, 1],
-    "classical julienne, plain (1)": [14, 1],
-    "russian pelmeni + beef and pork (1)": [18, 1],
-    "chicken tabaka (1)": [21, 1],
-    "tandoori large plate (1)": [69.99, 1],
-    "martini infused + apple and cinnamon (1)": [12, 1],
-    "dessert (6)": [round(42 / 6, 2), 6],
+    "drink: infused shot (13)": [6, 13],
+    "drink: lavender fusion (2)": [9, 2],
+    "drink: moskow mule (1)": [9, 1],
+    "meal: eggplant appetizer (2)": [8, 2],
+    "meal: vareniki + potato and mushrooms (2)": [17, 2],
+    "meal: fresh red salmon caviar karat (1)": [35, 1],
+    "meal: blini with stuffing (salmon) (1)": [12, 1],
+    "meal: classical julienne, plain (1)": [14, 1],
+    "meal: russian pelmeni + beef and pork (1)": [18, 1],
+    "meal: chicken tabaka (1)": [21, 1],
+    "meal: tandoori large plate (1)": [69.99, 1],
+    "drink: martini infused + apple and cinnamon (1)": [12, 1],
+    "dessert: halva (4)": [4.5, 4],
+    "dessert: kiev cake (1)": [8, 1],
+    "dessert: napoleon cake (1)": [1,1],
+    "dessert: medovik cake (1)": [1,1]
 }
 
 subtotal = 378.99
@@ -60,40 +62,64 @@ except:
         }
     )
 
+def name_chooser():
+    '''
+    Eliminates names that already occurred in the db
+    '''
+    all_names = ["Peter", "Matt", "Steve", "Aron", "Julie", "Grace", "Russell"]
+    # theres a bug where removing the name from names when names is the iteree,
+    # 
+    names = []
+    # remove names and meals from options if already in db
+    for name in all_names:
+        if name not in list(df_saved["name"].unique()):
+            names.append(name)
+    names.sort()
+    all_names.sort()
+    return all_names
 
-all_names = ["Peter", "Matt", "Steve", "Aron", "Julie", "Grace", "Russell"]
-# theres a bug where removing the name from names when names is the iteree,
-# 
-names = []
-# remove names and meals from options if already in db
-for name in all_names:
-    if name not in list(df_saved["name"].unique()):
-        names.append(name)
-names.sort()
+def meal_chooser():
+    '''
+    Eliminates meals that are in db + have been accounted for
+    '''
+    # number of times each meal was claimed
+    all_meals = [x.capitalize() for x in food_dict.keys()]
+    meals = all_meals.copy()
+    for meal in all_meals:
+        if meal in df_saved["food"].unique():
+            meal_df = df_saved[df_saved["food"] == meal]  # isolate df to just meal
+            amount_claimed = meal_df["amount"].sum()  # num times it was bought
+            if amount_claimed >= food_dict[meal.lower()][1]:
+                # if we reached how many times meal was bought, remove
+                # meal from available options
+                meals.remove(meal)
 
-# number of times each meal was claimed
-all_meals = [x.capitalize() for x in food_dict.keys()]
-meals = all_meals.copy()
-for meal in all_meals:
-    if meal in df_saved["food"].unique():
-        meal_df = df_saved[df_saved["food"] == meal]  # isolate df to just meal
-        amount_claimed = meal_df["amount"].sum()  # num times it was bought
-        if amount_claimed >= food_dict[meal.lower()][1]:
-            # if we reached how many times meal was bought, remove
-            # meal from available options
-            meals.remove(meal)
+    meals.sort()
+    return meals
 
-meals.sort()
-# gather user info
-name = st.selectbox("Name", options=names)
-order = st.multiselect("What did you order?", options=meals)
-amount_str = st.text_input("How much? (Ex: 1, 2, 0.5)")
+def info_gather(names, meals):
+    '''
+    Gathers info from user
+    '''
+    # gather user info
+    name = st.selectbox("Name", options=names)
+    colo, cola = st.beta_columns(2)
+    with colo:
+        order = st.selectbox("Select an order", options=meals)
+    with cola:
+        amount = st.number_input("How much? (Ex: 1, 2, 0.5)")
+    
+    return name, order, amount
 
+names = name_chooser()
+meals = meal_chooser()
+
+name, order, amount = info_gather(names, meals)
 ph = st.empty()
 
-if not amount_str or not order or not name:
+if not amount or not order or not name:
     st.info(
-        "Select all the foods you ordered or shared, and write the amount of each. If shared, use fractions."
+        "Select the food you ordered or shared, and write the amount of each. If shared, use fractions."
     )
     with ph.beta_container():
         st.write("__Current Database__")
@@ -101,25 +127,16 @@ if not amount_str or not order or not name:
     st.stop()
 
 # format inputs
-amount_list = amount_str.split(",")
-amount_list = [float(x.strip()) for x in amount_list]
-price = [food_dict[x.lower()][0] for x in order]
-
-# store inputs
-receipt_dict = {}
-for i in range(len(order)):
-    food = order[i]
-    amount = amount_list[i]
-    receipt_dict[food] = [amount]
+price = food_dict[order.lower()][0]
 
 # prettify inputs for user's benefit
 
 receipt_df = pd.DataFrame(
     {
-        "name": [name for i in range(len(receipt_dict.keys()))],
-        "food": receipt_dict.keys(),
-        "price": [food_dict[food.lower()][0] for food in receipt_dict.keys()],
-        "amount": [receipt_dict[food][0] for food in receipt_dict.keys()],
+        "name": [name],
+        "food": [order],
+        "price": [food_dict[order.lower()][0]],
+        "amount": [amount],
     }
 )
 
@@ -128,7 +145,7 @@ receipt_df["total_item_price"] = receipt_df["amount"] * receipt_df["price"]
 # calculate and present user's taxes, tips, subtotal, total
 user_subtotal = sum(receipt_df["total_item_price"])
 with ph.beta_container():
-    st.write("Your items")
+    st.write("__Your item__")
     st.table(receipt_df)
 
 total = user_subtotal + (user_subtotal * sales_tax_perc) + (user_subtotal * tip_perc)
@@ -136,15 +153,16 @@ st.info(
     f"""
 * Tax was {sales_tax_perc * 100}%, tip was {round((tip/subtotal) * 100,1)}%
 * Your subtotal is ${user_subtotal}
-* Your total is ${round(total,2)}"""
+* With tip and tax this meal cost ${round(total,2)}"""
 )
 
 # add to db
-if st.button("Confirm and Submit"):
+submit = st.button("Confirm and Submit")
+if submit:
     try:
         df_saved = df_saved.append(receipt_df)
         with ph.beta_container():
-            st.write("New Database")
+            st.write("__New Database__")
             st.table(df_saved)
         df_saved.to_sql("food", cnx, if_exists="replace", index=False)
         st.success("Saved to db!")
