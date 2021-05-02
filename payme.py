2132881b680342769ac509a601114a852d4a03df
Peter Gates <peter@Peters-MacBook-Pro.local>
2021-05-02 12:59:49 -0500

    finished receipt submission. Integrated into payme.

--- payme.py
+++ payme.py
@@ -21,81 +21,109 @@ footer {visibility: hidden;}
 </script>
 
 """
-st.set_page_config(page_title = 'Venmo Calculator')
+st.set_page_config(page_title="Venmo Calculator")
 # st.markdown(hide_streamlit_style, unsafe_allow_html=True) # hides the hamburger menu
 
-import streamlit_analytics
-
-with streamlit_analytics.track():
-    st.text_input("Write something")
-    st.button("Click me")
 
 def start():
-    '''
+    """
     Thalamus. Creates the GUI and redirects requests to the appropriate scripts. The Thalamus.
-    '''
-    select_input = 'release' # disabled user section of payme ('alpha' to activate)
-    service_chosen = st.select_slider("",options=['Delivery App','Manual Mode'])
-    
-    if 'Manual' not in service_chosen:
-        user_output = delivery_brain()
-    else:
-        user_output = mm.manual_mode()
-        
-    total_input, data = calc.total_calculator(**user_output)
-    # dictionary of kwargs for venmo_calc()
-    user_modified = {
-        'tax':user_output['tax_input'],
-        'tip':user_output['tip_input'],
-        'misc_fees':user_output['fees_input'],
-        'description':user_output['description'],
-        'total':total_input,
-        'discount':user_output['discount'],
-        'contribution':user_output['contribution'],
-        'my_dic':data
-    }
-    try:
-        calc_message = calc.venmo_calc(**user_modified)
-        calc.html_table(calc_message["messages"], calc_message["request_money"])
+    """
+    action = st.sidebar.radio(
+        "Choose an action", options=["Get venmo links", "Claim meals", "Submit receipt"]
+    )
 
-    except ZeroDivisionError:
-        st.info("See the how to for more information!")
-        calc_message={'request_money':None}
-        st.stop()
-        
+    if action == "Get venmo links":
+        select_input = "release"  # disabled user section of payme ('alpha' to activate)
+        service_chosen = st.sidebar.radio(
+            "Choose input type", options=["Delivery App", "Manual Mode"]
+        )
+
+        if "Manual" not in service_chosen:
+            user_output = delivery_brain()
+        else:
+            user_output = mm.manual_mode()
+
+        total_input, data = calc.total_calculator(**user_output)
+        # dictionary of kwargs for venmo_calc()
+        user_modified = {
+            "tax": user_output["tax_input"],
+            "tip": user_output["tip_input"],
+            "misc_fees": user_output["fees_input"],
+            "description": user_output["description"],
+            "total": total_input,
+            "discount": user_output["discount"],
+            "contribution": user_output["contribution"],
+            "my_dic": data,
+        }
+        try:
+            calc_message = calc.venmo_calc(**user_modified)
+            calc.html_table(calc_message["messages"], calc_message["request_money"])
+
+        except ZeroDivisionError:
+            st.info("See the how to for more information!")
+            calc_message = {"request_money": None}
+            st.stop()
+    elif action == "Claim meals":
+        from apps import food_select as fs
+
+        st.title("Claim your Meal!")
+        st.write("Let everyone know what you ordered and/or shared.")
+        sf = fs.labelFood()
+    else:
+        from apps import food_select as fs
+
+        st.title("Complex Receipt Receiver")
+        st.write("For when receipts are too complex to sort who got what")
+        rr = fs.receiptReceiver()
+
+
 def delivery_brain():
-    '''
+    """
     Instructions and web GUI to for web receipts, including logic to detect doordash vs ubereats.
-    '''
+    """
     ##########
     # HOW TO #
     ##########
     st.title("Venmo Requests Calculator: Delivery App Mode")
-    st.write("Give us the DoorDash or UberEats receipt, we'll spit out some venmo request links!")
+    st.write(
+        "Give us the DoorDash or UberEats receipt, we'll spit out some venmo request links!"
+    )
     with st.beta_expander("How To"):
-        col1,col2 = st.beta_columns(2)
+        col1, col2 = st.beta_columns(2)
         with col1:
-            st.write("""
+            st.write(
+                """
             __DoorDash__
 
             1. Copy and paste the entire contents of DoorDash receipt from __Order Details__ at the top to the __Total__ at the bottom.
-            2. Follow the prompts """)
+            2. Follow the prompts """
+            )
             st.write("")
-            st.markdown("![DoorDash copy instructions](https://github.com/pomkos/payme/raw/main/images/copy_dd.gif)")
+            st.markdown(
+                "![DoorDash copy instructions](https://github.com/pomkos/payme/raw/main/images/copy_dd.gif)"
+            )
         with col2:
-            st.write("""
+            st.write(
+                """
             __UberEats__
 
             1. Copy and paste the entire contents of UberEats receipt from __Total__ at the top to __Tip__ at the bottom.
             2. Once pasted, make sure names are on separate lines.
-            3. Follow the prompts""")
+            3. Follow the prompts"""
+            )
             st.write("")
-            st.markdown("![UberEats copy gif placeholder](https://github.com/pomkos/payme/raw/main/images/copy_ue.gif)")
+            st.markdown(
+                "![UberEats copy gif placeholder](https://github.com/pomkos/payme/raw/main/images/copy_ue.gif)"
+            )
     #######
     # GUI #
     #######
     description = st.text_input("(Optional) Description, like the restaurant name")
-    receipt = st.text_area("Paste the entire receipt from your service, including totals and fees", height = 300)
+    receipt = st.text_area(
+        "Paste the entire receipt from your service, including totals and fees",
+        height=300,
+    )
     receipt = receipt.lower()
 
     if not receipt:
@@ -106,41 +134,49 @@ def delivery_brain():
     # LOGIC #
     #########
     try:
-        if "(you)" in receipt.lower(): # ubereats has this
+        if "(you)" in receipt.lower():  # ubereats has this
             st.info("This looks like an UberEats receipt.")
             deny = st.checkbox("It's actually DoorDash")
             if deny:
-                service_chosen = 'doordash'
-            else: #its ubereats
-                service_chosen = 'ubereats'
-                receipt = receipt.replace(',','')
-        elif "participant" in receipt: # doordash
+                service_chosen = "doordash"
+            else:  # its ubereats
+                service_chosen = "ubereats"
+                receipt = receipt.replace(",", "")
+        elif "participant" in receipt:  # doordash
             st.info("This looks like a DoorDash receipt.")
             deny = st.checkbox("It's actually UberEats")
             if deny:
-                service_chosen = 'ubereats'
+                service_chosen = "ubereats"
             else:
-                service_chosen = 'doordash'
+                service_chosen = "doordash"
         else:
-            st.error("Unknown delivery app. See the how to, try the manual mode, or contact Pete to request support for the receipt!")
+            st.error(
+                "Unknown delivery app. See the how to, try the manual mode, or contact Pete to request support for the receipt!"
+            )
             st.stop()
-        my_names = mm.name_finder(receipt) # the name finder is located in manual_mode.py file
+        my_names = mm.name_finder(
+            receipt
+        )  # the name finder is located in manual_mode.py file
         service_chosen = service_chosen.lower()
-        if 'door' in service_chosen:
+        if "door" in service_chosen:
             from apps import doordash as dd
+
             user_output = dd.app(receipt, my_names, description)
-        elif 'uber' in service_chosen:
+        elif "uber" in service_chosen:
             from apps import ubereats as ue
+
             user_output = ue.app(receipt, my_names, description)
         return user_output
     except Exception as e:
         st.write(e)
         st.stop()
 
+
 def app():
-    '''
+    """
     Only purpose is to start the app from bash script. Own function so the rest of the functions can be organized in a logical way.
-    '''
+    """
     start()
+
 
 app()
